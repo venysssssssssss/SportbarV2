8842d5adaf878d38a5aff69bf5ecef18ccb8cd0e
venysssssssssss <shablaur7@hotmail.com>
2023-05-19 16:04:32 -0300

    alteração no action do form

--- cadastrar.php
+++ cadastrar.php
@@ -14,17 +14,48 @@ $u = new Usuario;
     <title>SportBar</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<div id="sign-up-form">
-    <form method="POST">
-        <label for="username">Nome</label>
-        <input type="text" name="username" maxlength="30" />
-        <label for="password">Senha</label>
-        <input id="password" type="password" name="password" maxlength="15" />
-        <label for="password0">Insira a senha novamente</label>
-        <input id="password0" type="password" name="password0" />
-        <input type="submit" class="button" name="submit" value="Cadastrar" />
-    </form>
-</div>
+<body>
+    <div class="container-shadow"></div>
+    <div class="container">
+      <div class="wrap">
+        <div class="headings">
+          <a id="sign-in" href="#" class="active"><span>Login</span></a>
+          <a id="sign-up" href="#"><span>Crie uma conta</span></a>
+        </div>
+        <div id="sign-in-form">
+          <form method="POST" action="processa.php">
+            <label for="username">Nome</label>
+            <input id="username" type="text" name="username" maxlength="30" />
+            <label for="password">Senha</label>
+            <input id="password" type="password" name="password" maxlength="15" />
+            <input id="remember" type="checkbox" />
+            <label for="remember" id="rlabel">Mantenha-me conectado</label>
+            <input type="submit" class="button" name="submit" value="Entrar" />
+          </form>
+  
+          <footer>
+            <div class="hr"></div>
+            <div class="fp"><a href="">Esqueceu a senha ?</a></div>
+          </footer>
+        </div>
+        <div id="sign-up-form">
+          <form method="POST">
+            <label for="username">Nome</label>
+            <input type="text" name="username" maxlength="30" />
+            <label for="password">Senha</label>
+            <input id="password" type="password" name="password" maxlength="15" />
+            <label for="password0">Insira a senha novamente</label>
+            <input id="password0" type="password" name="password0" />
+            <input type="submit" class="button" name="submit" value="Criar a conta" />
+          </form>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="assets/js/login.js"></script>
+      </div>
+    </div> 
+  </body>
+</html>
+
 
 <?php
     if(isset($_POST['submit']))
@@ -54,7 +85,7 @@ $u = new Usuario;
               {
                 ?>
                 <div class="msg-erro">
-                Usuário já cadastrado
+                Usuário já está cadastrado
                 </div>
                 <?php
               }
